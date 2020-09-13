46b0580a212368afd390ac4d98848b95aec25985
Cleyton Gama <cleytongama@gmail.com>
2020-09-13 14:28:08 -0300

    feat: cria setup do projeto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello Power2GO</Text>
+      <StatusBar />
+    </View>
+  );
+}
